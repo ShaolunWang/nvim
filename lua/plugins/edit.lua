38c9e0e2ece9f820dfd88fcf8cbba8ef2a8f69d5
ShaolunWang <wang.shaolun@gmail.com>
2024-11-05 02:16:05 -0800

    feat: add dial.nvim

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -19,7 +19,8 @@ return {
 	{
 		'numToStr/Comment.nvim',
 		opts = function()
-			local commentstring_avail, commentstring = pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
+			local commentstring_avail, commentstring = pcall(require,
+				'ts_context_commentstring.integrations.comment_nvim')
 			return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
 		end,
 		config = function()
@@ -83,6 +84,138 @@ return {
 		end,
 		cmd = { 'Rg', 'Sg' },
 	},
+	{
+		"monaqa/dial.nvim",
+		desc = "Increment and decrement numbers, dates, and more",
+		-- stylua: ignore
+		opts = function()
+			local augend = require("dial.augend")
+
+			local logical_alias = augend.constant.new({
+				elements = { "&&", "||" },
+				word = false,
+				cyclic = true,
+			})
+
+			local ordinal_numbers = augend.constant.new({
+				-- elements through which we cycle. When we increment, we go down
+				-- On decrement we go up
+				elements = {
+					"first",
+					"second",
+					"third",
+					"fourth",
+					"fifth",
+					"sixth",
+					"seventh",
+					"eighth",
+					"ninth",
+					"tenth",
+				},
+				-- if true, it only matches strings with word boundary. firstDate wouldn't work for example
+				word = false,
+				-- do we cycle back and forth (tenth to first on increment, first to tenth on decrement).
+				-- Otherwise nothing will happen when there are no further values
+				cyclic = true,
+			})
+
+			local weekdays = augend.constant.new({
+				elements = {
+					"Monday",
+					"Tuesday",
+					"Wednesday",
+					"Thursday",
+					"Friday",
+					"Saturday",
+					"Sunday",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			local months = augend.constant.new({
+				elements = {
+					"January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+					"July",
+					"August",
+					"September",
+					"October",
+					"November",
+					"December",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			local capitalized_boolean = augend.constant.new({
+				elements = {
+					"True",
+					"False",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			return {
+				dials_by_ft = {
+					css = "css",
+					javascript = "typescript",
+					javascriptreact = "typescript",
+					json = "json",
+					lua = "lua",
+					markdown = "markdown",
+					python = "python",
+					sass = "css",
+					scss = "css",
+					typescript = "typescript",
+					typescriptreact = "typescript",
+					yaml = "yaml",
+				},
+				groups = {
+					default = {
+						augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+						augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+						augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+						ordinal_numbers,
+						weekdays,
+						months,
+					},
+					markdown = {
+						augend.misc.alias.markdown_header,
+					},
+					lua = {
+						augend.integer.alias.decimal, -- nonnegative and negative decimal number
+						augend.constant.alias.bool, -- boolean value (true <-> false)
+						augend.constant.new({
+							elements = { "and", "or" },
+							word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+							cyclic = true, -- "or" is incremented into "and".
+						}),
+					},
+					python = {
+						augend.integer.alias.decimal, -- nonnegative and negative decimal number
+						capitalized_boolean,
+						logical_alias,
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("dial.config").augends:register_group(opts.groups)
+			vim.g.dials_by_ft = opts.dials_by_ft
+		end,
+		keys = {
+			{ "<C-a>",  function() return M.dial(true) end,        expr = true, desc = "Increment", mode = { "n", "v" } },
+			{ "<C-x>",  function() return M.dial(false) end,       expr = true, desc = "Decrement", mode = { "n", "v" } },
+			{ "g<C-a>", function() return M.dial(true, true) end,  expr = true, desc = "Increment", mode = { "n", "v" } },
+			{ "g<C-x>", function() return M.dial(false, true) end, expr = true, desc = "Decrement", mode = { "n", "v" } },
+		},
+	}
 	--[[ {
 		'chrisgrieser/nvim-various-textobjs',
 		opts = { useDefaultKeymaps = true },
