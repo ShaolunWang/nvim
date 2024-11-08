6bbce9a70634c8192706298b9110921c7d47837f
ShaolunWang <wang.shaolun@gmail.com>
2024-11-08 16:59:22 +0000

    markdown changes and fix keybind

--- lua/keymap/lsp_keymaps.lua
+++ lua/keymap/lsp_keymaps.lua
@@ -18,7 +18,7 @@ function M.on_attach(client, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, add_desc(bufopts, 'hover'))
 	vim.keymap.set('n', '\\D', vim.lsp.buf.type_definition, add_desc(bufopts, 'Jumps to type def'))
---	vim.keymap.set('n', '\\r', vim.lsp.buf.rename, add_desc(bufopts, 'Rename Variable'))
+	vim.keymap.set('n', '\\r', vim.lsp.buf.rename, add_desc(bufopts, 'Rename Variable'))
 	vim.keymap.set('n', '\\a', vim.lsp.buf.code_action, add_desc(bufopts, 'Code Action'))
 	vim.keymap.set('n', '\\q', vim.diagnostic.setloclist, add_desc(opts, 'Diag Loclist'))
 	vim.keymap.set('n', '\\f', function()
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -175,9 +175,9 @@ return {
 		},
 		keys = {
 			--		{ "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
-			{ ",f",  function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+			{ ",",  function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
 			--			{ "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
-			{ "\\r", function() Snacks.rename() end,                  desc = "Rename File" },
+--			{ "\\r", function() Snacks.rename() end,                  desc = "Rename File" },
 			{ "]]",  function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference" },
 			{ "[[",  function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" },
 		},
