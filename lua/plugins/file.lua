78c0f7022ca358cc325742c6be1893294bfae3b7
ShaolunWang <wang.shaolun@gmail.com>
2024-12-09 06:06:37 -0800

    feat(minor): re-enabled oil, added edge theme

--- lua/plugins/file.lua
+++ lua/plugins/file.lua
@@ -1,4 +1,60 @@
 return {
+	--[[ {
+		'nvim-tree/nvim-tree.lua',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('nvim-tree').setup(tree_opts)
+		end,
+		cmd = { 'NvimTreeToggle' },
+	},]]
+	{
+		'stevearc/oil.nvim',
+		opts = {
+			use_default_keymaps = false,
+			keymaps = {
+				['<CR>'] = 'actions.select',
+				['-'] = 'actions.parent',
+				['_'] = 'actions.open_cwd',
+				['`'] = 'actions.cd',
+				['~'] = 'actions.tcd',
+				['g.'] = 'actions.toggle_hidden',
+				['g?'] = false,
+				['<C-s>'] = false,
+				['<C-h>'] = false,
+				['<C-t>'] = false,
+				['<C-p>'] = false,
+				['<C-c>'] = false,
+				['<C-l>'] = false,
+				['gs'] = false,
+				['gx'] = false,
+				['g\\'] = false,
+			},
+		},
+		-- Optional dependencies
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		cmd = { 'Oil' },
+	},
+	--[[ {
+		'mikavilpas/yazi.nvim',
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			{
+				'<>',
+				'<cmd>Yazi<cr>',
+				desc = 'Open yazi at the current file',
+			},
+			{
+				-- Open in the current working directory
+				'<c-m>',
+				'<cmd>Yazi cwd<cr>',
+				desc = "Open the file manager in nvim's working directory",
+			},
+			},
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = true,
+		},
+	} ]]
 	{
 		'lambdalisue/vim-fern',
 		dependencies = {
