e44f478a7a2ccaa28828e9cf781604086c90ab33
ShaolunWang <wang.shaolun@gmail.com>
2025-04-21 03:11:43 -0700

    Merge main to ui

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -152,4 +152,25 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 		require('conform').format({ async = true, lsp_format = 'fallback' })
 	end),
 })
---vim.cmd[[autocmd! TermClose <buffer=abuf> if !v:event.status | exec 'bd! '..expand('<abuf>') | endif | checktime]]
+vim.cmd([[
+ augroup diffcolors
+     autocmd!
+     autocmd Colorscheme * call s:SetDiffHighlights()
+ augroup END
+
+
+
+ function! s:SetDiffHighlights()
+     if &background == "dark"
+         highlight DiffAdd gui=bold guifg=none guibg=#2e4b2e
+         highlight DiffDelete gui=bold guifg=none guibg=#4c1e15
+         highlight DiffChange gui=bold guifg=none guibg=#45565c
+         highlight DiffText gui=bold guifg=none guibg=#996d74
+     else
+         highlight DiffAdd gui=bold guifg=none guibg=palegreen
+         highlight DiffDelete gui=bold guifg=none guibg=tomato
+         highlight DiffChange gui=bold guifg=none guibg=lightblue
+         highlight DiffText gui=bold guifg=none guibg=lightpink
+     endif
+ endfunction
+]])
