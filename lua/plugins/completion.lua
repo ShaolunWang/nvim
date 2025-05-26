2e9fdbec43662721f73c717a810eede2e11cf91c
ShaolunWang <wang.shaolun@gmail.com>
2025-05-26 03:12:02 -0700

    misc(update): blink.cmp -> v1.3.1

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.plugins = {
-	{ 'saghen/blink.cmp', opt = true, branch = 'v1.1.1' },
+	{ 'saghen/blink.cmp', opt = true, branch = 'v1.3.1' },
 	{ 'mikavilpas/blink-ripgrep.nvim', opt = true },
 	{ 'saghen/blink.compat', opt = true },
 	{ 'xzbdmw/colorful-menu.nvim', opt = true },
