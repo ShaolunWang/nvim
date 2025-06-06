5d43a4e7bdf3e68938199d95b061d07ccbbfa136
ShaolunWang <wang.shaolun@gmail.com>
2025-06-06 17:26:01 +0100

    feat: add arglist helper function

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -183,6 +183,11 @@ function M.load()
 		},
 		{
 			'any-jump.vim',
+			after = function()
+				vim.g.any_jump_window_width_ratio = 0.8
+				vim.g.any_jump_window_height_ratio = 0.8
+				vim.g.any_jump_disable_default_keybindings = 1
+			end,
 			cmd = { 'AnyJump', 'AnyJumpArg', 'AnyJumpVisual', 'AnyJumpBack', 'AnyJumpLastResults' },
 		},
 	})
